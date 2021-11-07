83ae4589ba6283e79db4f9366a636e558e9cc898
AlexBones1 <zergovii@yandex.ru>
2021-11-07 11:45:44 +0500

    grop _ to _ login

---
+++ group.py
@@ -0,0 +1,7 @@
+class Group:
+
+    def __init__(self, firstname, surname, age, notes):
+        self.firstname = 'Alex'
+        self.surname = 'Trax'
+        self.age = '28'
+        self.notes = 'notes'
